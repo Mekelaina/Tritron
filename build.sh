bdf18f7601d884b26265e228aef136c0a85222a7
Mekelaina <truemr.derpster@gmail.com>
2025-09-19 18:42:43 -0400

    linking with math library added to build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 #gcc src/tritron.c src/trit.c src/trit.h -o tritron -Wall -Wextra --verbose --pedantic
 #gcc src/tritron.c src/trit.c src/trit.h -o tritron -Wall -Wextra 
-gcc src/*.c src/*.h -o tritron -Wall -Wextra 
+gcc src/*.c src/*.h -o tritron -Wall -Wextra -lm
